50e7bc35b5a09303c96044e3a4e845c850fcf7e9
Vishux777 <vishwas.23@lpu.in>
2024-09-25 19:54:33 +0530

    Feat: Calling Private Var using Pointer

---
+++ Calling Private Var using Pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class MyClass {
+private:
+    int privateVar;
+public:
+    MyClass(int value) : privateVar(value) {}
+
+    void printPrivateVar() const {
+        cout << "Private variable value: " << privateVar << endl;
+    }
+};
+int main() {
+    int input;
+    cout << "Enter a number: ";
+    cin >> input;
+    MyClass obj(input);
+    int* p = (int*)&obj;
+    cout <<*p << endl;
+
+    return 0;
+}
