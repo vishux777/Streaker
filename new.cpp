44757367b20119575ffc1ccdd83d499e1e6e1e0b
vishux777 <vishwas.23@lpu.in>
2024-10-14 19:31:31 +0530

    template

--- new.cpp
+++ new.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-template <class T>;
+template <class T>
 class calculate{
     private:
     T x,y;
@@ -18,7 +18,10 @@ class calculate{
 int main(){
     /*calculate obj1(2,5);
     obj1.add();*/
-    #new way----------------------------
+    /*new way----------------------------*/
 
-    
+    calculate<int> obj1(5,7);
+    obj1.add();
+    calculate<double> obj2(4.7,8.2);
+    obj2.add();
 }
