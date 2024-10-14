9d26b69061270d19a50bad660f9acc55a4fe4af5
vishux777 <vishwas.23@lpu.in>
2024-10-14 19:53:48 +0530

    template

---
+++ template for int fn.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+T add(T num1,T num2){
+    return(num1+num2);
+}
+int main(){
+    cout<<add<int>(2,4);
+    cout<<add<string>("\nbhai","hello");
+}
